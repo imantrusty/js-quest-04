e28b171a50c60a60ec9ac9c43a08b2608550c81b
Iman <133013685+imantrusty@users.noreply.github.com>
2023-06-05 12:29:06 -0500

    Add files via upload

---
+++ ex00/my_first_script.js
@@ -0,0 +1,6 @@
+
+    function print(){
+    console.log("Hello World!");
+    }
+    print();
+
---
+++ ex03/my_first_function.js
@@ -0,0 +1,6 @@
+
+// Following XXXXXX will be a function that will print using console.log("my_first_function")
+function my_first_function(){
+console.log("my_first_function");
+}
+my_first_function();
